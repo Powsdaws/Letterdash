599f4bf75990401c7368399e35a21a0de7d22402
Marius Holm <marho@itu.dk>
2025-09-17 21:01:52 +0200

    feat: added tailwind

--- frontend/frontend/src/App.vue
+++ frontend/frontend/src/App.vue
@@ -3,7 +3,10 @@ import GameBoard from './components/GameBoard.vue'
 </script>
 
 <template>
-  <p>hi</p>
+  <p  class="font-bold">hi</p>
+  <div class="bg-green-500 text-white p-4 rounded">
+    Tailwind is working!
+  </div>
   <GameBoard></GameBoard>
 </template>
 
