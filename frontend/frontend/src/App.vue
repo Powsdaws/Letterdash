1bf71b8a56064f3e13c42331c6331840141955c4
Marius Holm <marho@itu.dk>
2025-09-17 21:32:30 +0200

    feat: basic gameboard layout

--- frontend/frontend/src/App.vue
+++ frontend/frontend/src/App.vue
@@ -3,10 +3,6 @@ import GameBoard from './components/GameBoard.vue'
 </script>
 
 <template>
-  <p  class="font-bold">hi</p>
-  <div class="bg-green-500 text-white p-4 rounded">
-    Tailwind is working!
-  </div>
   <GameBoard></GameBoard>
 </template>
 
--- frontend/frontend/src/components/GameBoard.vue
+++ frontend/frontend/src/components/GameBoard.vue
@@ -2,10 +2,18 @@
 export default{}
 </script>
 
+<!-- Should be the guesses-->
+
 <template>
-    <p>a</p>
+    <div class="flex flex-row min-h-screen justify-center items-center content-start gap-4">
+        <div class="flex flex-col w-[35vw] h-[70vh] border-4">
+            <p class="text-center p-5 font-bold">Guesses</p>
+            <p class="text-center">-guess1-</p>
+        </div>
+    </div>
 </template>
 
 
 
 
+S
