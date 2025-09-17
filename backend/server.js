8becaa31cfcb47b25de5216e11cee45e079c7267
Marius Holm <marho@itu.dk>
2025-09-17 19:54:07 +0200

    feat: init frontend

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.get("/api/word", (req, res) => {
 mongoose.connect("mongodb://localhost:27017/wordle");
 
 //Starts the server/app
-const port = process.env.port || 3000;
+const port = process.env.port || 5000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 }) 
