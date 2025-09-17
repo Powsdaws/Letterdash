343759a8d898b703fb4a66de7b265548098ac7cf
Marius Holm <marho@itu.dk>
2025-09-17 19:49:16 +0200

    feat: connect to mongoDB

---
+++ backend/models/Word.js
@@ -0,0 +1,10 @@
+// models/Word.js
+import mongoose from "mongoose";
+
+const WordSchema = new mongoose.Schema({
+  word: { type: String, required: true }
+});
+
+const Word = mongoose.model("Word", WordSchema);
+
+export default Word;
