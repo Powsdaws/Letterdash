56a74c17e53a3852699f450e8c7a48817b3ba1cb
Marius Holm <marho@itu.dk>
2025-09-20 17:03:42 +0200

    feat: change color based on correctness

--- frontend/frontend/src/components/WordGuess/GameBoard.vue
+++ frontend/frontend/src/components/WordGuess/GameBoard.vue
@@ -4,8 +4,13 @@ defineProps({
     guesses: {
         type: Array,
         required: true //the prop must be given to init this component
-    }
+    },
+    rowStatus: Array,
+    currentRow: Number
 })
+
+
+
 
 </script>
 
@@ -16,7 +21,13 @@ defineProps({
         <p class="font-bold text-5xl">Guesses</p>
         <div class="grid gap-2">
             <div v-for="(guess, i) in guesses" :key="i" class="grid grid-cols-5 gap-2">
-                <span v-for="n in 5" :key="n"class="flex items-center justify-center w-12 h-12 border border-gray-400 text-xl font-bold uppercase">
+                <span 
+                class="flex items-center justify-center w-12 h-12 border-4 text-xl font-bold uppercase"
+                :class="{
+                    'border-green-400' : rowStatus?.[i] === 'correct', 
+                    'border-red-400' : rowStatus[i] === 'incorrect',
+                    ' border-gray-600' : rowStatus[i] === ''}" 
+                    v-for="n in 5" :key="n">
                     {{ guess[n - 1] || "_" }}
                 </span>
             </div>
--- frontend/frontend/src/components/WordGuess/WordGuess.vue
+++ frontend/frontend/src/components/WordGuess/WordGuess.vue
@@ -5,6 +5,8 @@ import Keyboard from './Keyboard.vue';
 
 const guesses = ref(["","","","","", ""]) //The guesses of the player
 const currentRow = ref(0);
+const rowStatus = ref(['','','','','', '']);
+const solution = ref("AAAAA");
 
 function handleKeyPress(letter) {
     //add letter to the current row
@@ -21,6 +23,13 @@ function handleDeleteLetter() {
 
 function handleEnter() {
     if (guesses.value[currentRow.value].length === 5) {
+        if (guesses.value[currentRow.value].toUpperCase() === solution.value.toUpperCase()) {
+            rowStatus.value[currentRow.value] = "correct"
+            console.log("NICELY DONE!")
+        } else {
+            rowStatus.value[currentRow.value] = "incorrect"
+        }
+        
         currentRow.value++;
     }
 }
@@ -29,7 +38,7 @@ function handleEnter() {
 
 <template>
     <div class="flex flex-col min-h-screen justify-center items-center content-start gap-4 ">
-        <GameBoard :guesses="guesses"></GameBoard>
+        <GameBoard :guesses="guesses" :rowStatus="rowStatus" :currentRow="currentRow"></GameBoard>
         <Keyboard
         @key-press="handleKeyPress"
         @delete="handleDeleteLetter"
