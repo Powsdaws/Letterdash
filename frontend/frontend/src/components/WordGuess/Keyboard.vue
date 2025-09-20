f7b9021ea6a70ded5d5dab1dbc559726f07dd1b4
Marius Holm <marho@itu.dk>
2025-09-20 17:15:17 +0200

    feat: changed UI for keyboard

--- frontend/frontend/src/components/WordGuess/Keyboard.vue
+++ frontend/frontend/src/components/WordGuess/Keyboard.vue
@@ -9,13 +9,17 @@ function pressKey(letter) {
 </script>
 
 <template>
-    <div class="keyboard">
+    <div class="grid grid-cols-10 gap-2">
         <button v-for="letter in 'QWERTYUIOPASDFGHJKLZXCVBNM'"
         :key = "letter"
-        @click="pressKey(letter)">{{ letter }}
+        @click="pressKey(letter)"
+        class="border outline-offset-2 outline-sky-500 focus:outline-2 size-12">
+        <span >
+            {{ letter }}
+        </span>
         </button>
-
         <button @click="emit('enter')">Enter</button>
         <button @click="emit('delete')">Delete</button>
     </div>
+    
 </template>
