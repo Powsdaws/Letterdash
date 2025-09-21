af33650bd9ef5d1dbfd5a6a769a6c5d6f89347f8
Marius Holm <marho@itu.dk>
2025-09-21 16:13:15 +0200

    feat: added primvue

--- frontend/frontend/src/main.ts
+++ frontend/frontend/src/main.ts
@@ -4,4 +4,13 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//Primevue
+import PrimeVue from 'primevue/config'
+import 'primevue/resources/themes/lara-light-blue/theme.css'  // or another Lara theme
+import 'primevue/resources/primevue.min.css'             
+import 'primeicons/primeicons.css'                       
+
+const app = createApp(App)
+app.use(PrimeVue)
+
+app.mount('#app')
