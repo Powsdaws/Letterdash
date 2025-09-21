d317eb29bd27bdc2ad719d4a3aa2b0527a2d50d7
Marius Holm <marho@itu.dk>
2025-09-21 16:26:19 +0200

    fix: fix primvue imports

--- frontend/frontend/src/main.ts
+++ frontend/frontend/src/main.ts
@@ -5,10 +5,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 //Primevue
-import PrimeVue from 'primevue/config'
-import 'primevue/resources/themes/lara-light-blue/theme.css'  // or another Lara theme
-import 'primevue/resources/primevue.min.css'             
-import 'primeicons/primeicons.css'                       
+import PrimeVue from "primevue/config";
+import Aura from "@primeuix/themes/aura";   // Or Lara, Material, Nora, etc.
+import "primeicons/primeicons.css";                       
 
 const app = createApp(App)
 app.use(PrimeVue)
