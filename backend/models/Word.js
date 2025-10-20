a10a151a26f34aaf17f24c44e37cf31d2fadcc8f
Marius Holm <marho@itu.dk>
2025-10-20 18:02:25 +0200

    chore: comment

--- backend/models/Word.js
+++ backend/models/Word.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose"
 
+//Object Data Modeling (ODM) library - allows to interact with the Word collection (table in db)
+
 const WordSchema = new mongoose.Schema({
   word: { type: String, required: true }
 })
