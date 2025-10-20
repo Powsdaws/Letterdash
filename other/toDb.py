e0f5a1656168bd140a2157921fcfd6f7fe527906
Marius Holm <marho@itu.dk>
2025-10-20 21:56:11 +0200

    chore: update date in word data

--- other/toDb.py
+++ other/toDb.py
@@ -5,7 +5,7 @@ input_file = "words.txt"
 output_file = "words.csv"
 
 # Start from 20 October 2025
-start_date = datetime.strptime("20-10-2025", "%d-%m-%Y")
+start_date = datetime.strptime("19-10-2025", "%d-%m-%Y")
 
 with open(input_file, "r") as f, open(output_file, "w", newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
